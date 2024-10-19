aa4e07490659b6c180b7c61759ee31eafa075082
Robert Gleason <l2ob122@gmail.com>
2024-10-18 21:11:19 -0700

    created basic hero layout and added svelte config changes

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,12 @@
+<section class="hero-section default margin">
+	<h3>Hello! I'm Rob</h3>
+	<div class="underscore"></div>
+	<h1>I'm a Fullstack developer creating modern</h1>
+	<h1 class="dark-grey">web solutions</h1>
+</section>
+
+<style>
+	.hero-section {
+		padding-top: 60px;
+	}
+</style>
