45374905ee12b37e97ef30321c8931791b00f58e
Robert Gleason <l2ob122@gmail.com>
2024-10-18 21:29:46 -0700

    created a resuable button and added basic styling

---
+++ src/lib/components/sections/HeroSection.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Button from '$components/Button.svelte';
+
+	function onclick() {
+		goto('/#contact-form');
+	}
+</script>
+
+<section class="hero-section default-margin">
+	<h3>Hello! I'm Rob</h3>
+	<div class="underscore"></div>
+	<h1>I'm a Fullstack developer creating modern</h1>
+	<h1 class="dark-grey">web solutions</h1>
+	<Button {onclick}>Let's Talk!</Button>
+</section>
+
+<style>
+	.hero-section {
+		padding-top: 60px;
+	}
+</style>
